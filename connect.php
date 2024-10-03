91755bb32e43dd8ae57ab03a5b945ac30d6018d2
adamoore <moore.adasha@gmail.com>
2024-10-03 11:35:56 +0300

    portti lisätty

--- connect.php
+++ connect.php
@@ -3,6 +3,7 @@ $servername = "127.0.0.1"; // Korvaa tämä palvelimen IP-osoitteella
 $username = "hasse"; // Korvaa tämä luomallasi käyttäjänimellä
 $password = "salasana"; // Korvaa tämä luomallasi salasanalla
 $dbname = "kalenteri"; // Korvaa tämä tietokannan nimellä
+$port = 3306; // Portti, oletuksena 3306
 
 // Luo yhteys
 $conn = new mysqli($servername, $username, $password, $dbname);
