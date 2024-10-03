3193aa857cbcbae7a77b133d999378e2f91cc31e
adamoore <moore.adasha@gmail.com>
2024-10-03 11:15:45 +0300

    tietokantayhteys

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "127.0.0.1"; // Korvaa tämä palvelimen IP-osoitteella
+$username = "hasse"; // Korvaa tämä luomallasi käyttäjänimellä
+$password = "salasana"; // Korvaa tämä luomallasi salasanalla
+$dbname = "kalenteri"; // Korvaa tämä tietokannan nimellä
+
+// Luo yhteys
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Tarkista yhteys
+if ($conn->connect_error) {
+    die("Yhteys epäonnistui: " . $conn->connect_error);
+}
+echo "Yhteys onnistui";
+?>
