4b361ae4e703ebdcff4b28247aa552184c7a604b
adamoore <moore.adasha@gmail.com>
2024-10-10 14:18:36 +0300

    10.10.

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <br>
 <div class="footer-icons">
     
-    <a href="https://www.youtube.com/@msorbiit" target="_blank"a>
-        <img
-        src="msorbiitkuvat/youtubepyöreä.png" alt="youtube" class="ikonit" />
-    </a>
-
     <a href="https://www.tiktok.com/@annikaredsven?lang=fi-FI" target="_blank">
         <img
         src="msorbiitkuvat/tiktokpyöreä.png" alt="tiktok" class="ikonit" />
     </a>
 
+    <a href="https://www.youtube.com/@msorbiit" target="_blank"a>
+        <img
+        src="msorbiitkuvat/youtubepyöreä.png" alt="youtube" class="ikonit" />
+    </a>
+    
     <a href="https://www.instagram.com/ms_orbiit/" target="_blank"a>
         <img
         src="msorbiitkuvat/instagrampyöreä.png" alt="instagram" class="ikonit" />
